6dcef07882e331d2c8f4803b32666baf467c5f98
cmasaitis <cmasaitis@TACs-MacBook-Pro.local>
2018-10-17 19:16:03 -0700

    stage

--- js/randomGeneratorsVER2.js
+++ js/randomGeneratorsVER2.js
@@ -2,40 +2,49 @@
 // TODO: Use constructor notation to create an object
 // with properties and methods that can be shared by both
 // diceRollGenerator and eightBallGenerator.
-function Generator(value) {
-  // YOUR CODE HERE
-  this.value = Math.floor((Math.random() * 6) + 1);
+function Generator(opciones) {
 
+  this.getValue = function(){
+    var value = Math.floor((Math.random() * opciones.length));
+    var answer = opciones[value];
+    return(answer);
+  }
 
+  this.addresult = function(newSides) {
+    var diff = newSides - (opciones.length + 1);
+      while(diff > 0){
+        opciones.push(opciones.length + 2);
+        diff --;
+      }
+
+        while(diff < -1){
+          opciones.pop();
+          diff ++;
+
+      }
+    }
 }
-
 // TODO: Initialise diceRollGenerator and eightBallGenerator
 // using the constructor notation and the Generator object
 // you just created
-var diceRollGenerator = new Generator();
-var eightBallGenerator = new Generator();
-
-console.log (diceRollGenerator);
-console.log (eightBallGenerator);
+var diceRollGenerator = new Generator([1, 2, 3, 4, 5, 6]);
+var eightBallGenerator = new Generator(['Indubitably', 'IDK my dude', 'Probs not', 'Hahaha no', 'Why are you asking a magic eight ball?', 'Figure it out yourself dingus']);
 
 function handleDiceRoll() {
   const resultElement = document.getElementById('diceResult');
 
   // TODO: call a method on diceRollGenerator to populate result with a random value
-  const result = diceRollGenerator.value;
+  const result = diceRollGenerator.getValue();
   resultElement.innerHTML = result;
 
-  diceRollGenerator = new Generator();
 }
 
 function handleEightBallShake() {
   const resultElement = document.getElementById('eightBallResult');
-  var answers = ['Indubitably', 'IDK my dude', 'Probs not', 'Hahaha no', 'Why are you asking a magic eight ball?', 'Figure it out yourself dingus'];
   // TODO: call a method on eightBallGenerator to populate result with a random value
-  const result = answers[eightBallGenerator.value - 1];
+  const result = eightBallGenerator.getValue();
   resultElement.innerHTML = result;
 
-  eightBallGenerator = new Generator();
 }
 
 document.addEventListener("DOMContentLoaded", function() {
--- js/randomGeneratorsVER3.js
+++ js/randomGeneratorsVER3.js
@@ -2,40 +2,58 @@
 // TODO: Use constructor notation to create an object
 // with properties and methods that can be shared by both
 // diceRollGenerator and eightBallGenerator.
-function Generator(value) {
-  // YOUR CODE HERE
-  this.value = Math.floor((Math.random() * 6) + 1);
+function Generator(opciones) {
 
+  this.getValue = function(){
+    var value = Math.floor((Math.random() * opciones.length));
+    var answer = opciones[value];
+    return(answer);
+  }
 
+  this.addresult = function(newSides) {
+    var diff = newSides - (opciones.length + 1);
+      while(diff > 0){
+        opciones.push(opciones.length + 2);
+        diff --;
+      }
+
+        while(diff < -1){
+          opciones.pop();
+          diff ++;
+
+      }
+    }
 }
-
 // TODO: Initialise diceRollGenerator and eightBallGenerator
 // using the constructor notation and the Generator object
 // you just created
-var diceRollGenerator = new Generator();
-var eightBallGenerator = new Generator();
+var diceRollGenerator = new Generator([1, 2, 3, 4, 5, 6]);
+var eightBallGenerator = new Generator(['Indubitably', 'IDK my dude', 'Probs not', 'Hahaha no', 'Why are you asking a magic eight ball?', 'Figure it out yourself dingus']);
+var slotMachineGenerator = new Generator(['Parsnips', 'BANKRUPT', 'Manilla Wafers', 'Try Again?', '777', 'Cherries!']);
 
-console.log (diceRollGenerator);
-console.log (eightBallGenerator);
 
 function handleDiceRoll() {
   const resultElement = document.getElementById('diceResult');
 
   // TODO: call a method on diceRollGenerator to populate result with a random value
-  const result = diceRollGenerator.value;
+  const result = diceRollGenerator.getValue();
   resultElement.innerHTML = result;
 
-  diceRollGenerator = new Generator();
 }
 
 function handleEightBallShake() {
   const resultElement = document.getElementById('eightBallResult');
-  var answers = ['Indubitably', 'IDK my dude', 'Probs not', 'Hahaha no', 'Why are you asking a magic eight ball?', 'Figure it out yourself dingus'];
   // TODO: call a method on eightBallGenerator to populate result with a random value
-  const result = answers[eightBallGenerator.value - 1];
+  const result = eightBallGenerator.getValue();
   resultElement.innerHTML = result;
 
-  eightBallGenerator = new Generator();
+}
+
+function handleSlotMachineShake() {
+  const resultElement = document.getElementById('slotMachineResult');
+  const result = slotMachineGenerator.getValue();
+  resultElement.innerHTML = result;
+
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -50,5 +68,10 @@ document.addEventListener("DOMContentLoaded", function() {
   const eightBallButton = document.getElementById('eightBallButton');
   if (eightBallButton) {
     eightBallButton.addEventListener('click', handleEightBallShake);
+  }
+
+  const slotMachineButton = document.getElementById('slotMachineButton');
+  if (slotMachineButton) {
+    slotMachineButton.addEventListener('click', handleSlotMachineShake);
   }
 });
